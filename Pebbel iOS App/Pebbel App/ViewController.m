a6a02d02f61a6e6f66f16150440c44b73214b4fe
Tobias Ottenweller <tottenweller@babbel.com>
2015-04-16 18:09:44 +0200

    Set app UUID.

--- Pebbel iOS App/Pebbel App/ViewController.m
+++ Pebbel iOS App/Pebbel App/ViewController.m
@@ -20,7 +20,9 @@
     // setup connection to watch and watch app
     self.watch = [[PBPebbleCentral defaultCentral] lastConnectedWatch];
     uuid_t myAppUUIDbytes;
-    NSUUID *myAppUUID = [[NSUUID alloc] initWithUUIDString:@""];
+    NSUUID *myAppUUID = [[NSUUID alloc] initWithUUIDString:@"186126b1-f071-4659-8270-220a8140d095"];
+    [myAppUUID getUUIDBytes:myAppUUIDbytes];
+    [[PBPebbleCentral defaultCentral] setAppUUID:[NSData dataWithBytes:myAppUUIDbytes length:sizeof(myAppUUIDbytes)]];
 }
 
 
@@ -29,7 +31,7 @@
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     NSString *text = textField.text;
-    NSDictionary *update = @{ @"text": text };
+    NSDictionary *update = @{ @1: text };
     
     [self.watch appMessagesPushUpdate:update
                                onSent:^(PBWatch *watch, NSDictionary *update, NSError *error) {
